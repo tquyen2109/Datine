bf36e4c5b674aae0f3309de5469ade4a70197e53
Quyen Tran <t.quyen1997@yahoo.com>
2020-05-24 08:56:17 -0400

    Add Database Project

---
+++ DatingAppDatabase/dbo/Tables/Values.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Values] (
+    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Values] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
