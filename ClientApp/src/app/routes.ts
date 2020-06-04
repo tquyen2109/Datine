f9498dc8747ec58e97b8925d6b4a50384d59fc6c
Quyen Tran <t.quyen1997@yahoo.com>
2020-06-04 19:25:17 -0400

    Add Route Guards

--- ClientApp/src/app/routes.ts
+++ ClientApp/src/app/routes.ts
@@ -3,11 +3,19 @@ import { MessagesComponent } from './messages/messages.component';
 import { HomeComponent } from './home/home.component';
 import {Routes} from '@angular/router';
 import { MemberListComponent } from './member-list/member-list.component';
+import { AuthGuard } from './_guards/auth.guard';
 export const appRoutes: Routes = [
-    {path: 'home', component: HomeComponent},
-    {path: 'members', component: MemberListComponent},
-    {path: 'messages', component: MessagesComponent},
-    {path: 'list', component: ListsComponent},
-    {path: '**', redirectTo: 'home', pathMatch: 'full'}
+    {path: '', component: HomeComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            {path: 'members', component: MemberListComponent},
+            {path: 'messages', component: MessagesComponent},
+            {path: 'list', component: ListsComponent},
+        ]
+    }, 
+    {path: '**', redirectTo: '', pathMatch: 'full'},
 ];
 
