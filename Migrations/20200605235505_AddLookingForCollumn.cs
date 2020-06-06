b3ab5f8c5ead7d48068e8a11cfde970e02b1e685
Quyen Tran <t.quyen1997@yahoo.com>
2020-06-06 19:41:43 -0400

    Add LookFor Collumn annd Inject Seed service

---
+++ Migrations/20200605235505_AddLookingForCollumn.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApplication.Migrations
+{
+    public partial class AddLookingForCollumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LookingFor",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LookingFor",
+                table: "Users");
+        }
+    }
+}
--- Model/User.cs
+++ Model/User.cs
@@ -20,6 +20,7 @@ namespace DatingApplication.Model
         public string Interest  { get; set; }
         public string City { get; set; }
         public string Country  { get; set; }
+        public string LookingFor { get; set; }
         public ICollection<Photo> Photos { get; set; }
     }
 }
