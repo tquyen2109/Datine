8319e9e6650da50b93e21ee425225cd1e1738ee0
Quyen Tran <t.quyen1997@yahoo.com>
2020-06-15 13:44:01 -0400

    Develop register section client side

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
@@ -17,6 +17,7 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { BsDropdownModule} from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
@@ -56,7 +57,10 @@ export class CustomHammerConfig extends HammerGestureConfig  {
       BrowserModule,
       HttpClientModule,
       FormsModule,
+      ReactiveFormsModule,
       BrowserAnimationsModule,
+      BrowserAnimationsModule,
+      BsDatepickerModule.forRoot(),
       BsDropdownModule.forRoot(),
       TabsModule.forRoot(),
       AngularFontAwesomeModule,
--- ClientApp/src/app/register/register.component.ts
+++ ClientApp/src/app/register/register.component.ts
@@ -2,6 +2,8 @@ import { AlertifyService } from './../_services/alertify.service';
 import { AuthService } from './../_services/auth.service';
 import { Component, OnInit, Input, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-register',
@@ -10,19 +12,41 @@ import { EventEmitter } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
   model: any = {};
+  registerForm: FormGroup;
+  bsConfig: Partial<BsDatepickerConfig>;
   @Output() cancellRegister = new EventEmitter();
   constructor(private authService: AuthService,
-              private alertify: AlertifyService) { }
+              private alertify: AlertifyService,
+              private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.bsConfig = {
+      containerClass: 'theme-red'
+    };
+   this.createRegisterForm();
+  }
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: [null, Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', Validators.required]
+    }, {validator: this.passwordMatchValidator});
+  }
+  passwordMatchValidator(g: FormGroup){
+    return g.get('password').value === g.get('confirmPassword').value ? null : {'mismatch': true};
   }
   register(){
-   this.authService.register(this.model).subscribe(() => {
-     this.alertify.success('registration successful')
-   }, error => {
-     this.alertify.error(error);
-   });
-
+  //  this.authService.register(this.model).subscribe(() => {
+  //    this.alertify.success('registration successful')
+  //  }, error => {
+  //    this.alertify.error(error);
+  //  });
+    console.log(this.registerForm.value);
   }
   cancel() {
     this.cancellRegister.emit(false);
