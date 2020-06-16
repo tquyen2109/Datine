144ab3e18e210d7614529c4deb9dceb10df081ba
Quyen Tran <t.quyen1997@yahoo.com>
2020-06-16 11:45:34 -0400

    Filter API

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -29,6 +29,13 @@ namespace DatingApplication.Controllers
         [HttpGet]
         public async Task<IActionResult> GetUsers([FromQuery]UserParams userParams )
         {
+            var currentUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            var userFromRepo = await _repo.GetUser(currentUserId);
+            userParams.UserId = currentUserId;
+            if(string.IsNullOrEmpty(userParams.Gender))
+            {
+                userParams.Gender = userFromRepo.Gender == "male" ? "female" : "male";
+            }
             var users = await _repo.GetUsers(userParams);
             var usersToReturn = _mapper.Map<List<UserForList>>(users);
             Response.AddPagination(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages);
