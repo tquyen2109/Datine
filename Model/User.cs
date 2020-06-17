f7ef296847c28d84ea79b8e53383115596a363c6
Quyen Tran <t.quyen1997@yahoo.com>
2020-06-17 16:21:41 -0400

    Add Messages Entity

--- Model/User.cs
+++ Model/User.cs
@@ -24,5 +24,7 @@ namespace DatingApplication.Model
         public ICollection<Photo> Photos { get; set; }
         public ICollection<Like> Likers { get; set; }
         public ICollection<Like> Likees { get; set; }
+        public ICollection<Message> MessagesSent { get; set; }
+        public ICollection<Message> MessagesReceived { get; set; }
     }
 }
